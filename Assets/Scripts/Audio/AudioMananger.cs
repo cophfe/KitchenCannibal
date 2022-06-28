485a25d05744511567da1c6e9747adf0a899c48b
DecJam <Declan@thedollerfamily.net>
2022-06-28 13:49:54 +1000

    small changes

--- Assets/Scripts/Audio/AudioMananger.cs
+++ Assets/Scripts/Audio/AudioMananger.cs
@@ -5,33 +5,52 @@ using UnityEngine;
 public enum SoundSources
 {
     Bone,
-    HealthInspector,
+    Bread,
+    Cloche,
     CupBoard,
     CuttingBoard,
-    Torso,
-    Limb,
     Fridge,
-    Order,
     GameState,
+    Grill,
+    Grinder,
+    HealthInspector,
+    Knife,
+    Limb, 
     Meat,
+    Music,
+    Order,
+    Plate,
+    Hand,
+    Salad,
+    Torso,
     Vegetable
 }
 
 public class AudioMananger : MonoBehaviour
 {
     public AudioSource mainAudioSource = null;
-    [HideInInspector] public List<AudioClip>[] clipList = new List<AudioClip>[11];
+    private List<AudioClip>[] clipList = new List<AudioClip>[20];
     [SerializeField] private List<AudioClip> boneClips;
-    [SerializeField] private List<AudioClip> healthInspectorClips;
+    [SerializeField] private List<AudioClip> breadClips;
+    [SerializeField] private List<AudioClip> clocheClips;
     [SerializeField] private List<AudioClip> cupboardClips;
     [SerializeField] private List<AudioClip> cuttingboardClips;
-    [SerializeField] private List<AudioClip> torsoClips;
-    [SerializeField] private List<AudioClip> limbClips;
     [SerializeField] private List<AudioClip> fridgeClips;
-    [SerializeField] private List<AudioClip> orderClips;
-    [SerializeField] private List<AudioClip> gameStateClips;
+    [SerializeField] private List<AudioClip> gamestateClips;
+    [SerializeField] private List<AudioClip> grillClips;
+    [SerializeField] private List<AudioClip> grinderClips;
+    [SerializeField] private List<AudioClip> healthInspectorClips;
+    [SerializeField] private List<AudioClip> knifeClips;
+    [SerializeField] private List<AudioClip> limbClips;
     [SerializeField] private List<AudioClip> meatClips;
+    [SerializeField] private List<AudioClip> musicClips;
+    [SerializeField] private List<AudioClip> orderClips;
+    [SerializeField] private List<AudioClip> plateClips;
+    [SerializeField] private List<AudioClip> handClips;
+    [SerializeField] private List<AudioClip> saladClips;
+    [SerializeField] private List<AudioClip> torsoClips;
     [SerializeField] private List<AudioClip> vegetableClips;
+    
 
     /// <summary>
     /// fills the array of lists with thir respective lists.
@@ -41,18 +60,27 @@ public class AudioMananger : MonoBehaviour
         mainAudioSource = GetComponent<AudioSource>();
         if (mainAudioSource == null)
             Debug.LogError("Missing component 'AudioSource'");
-        
+
         clipList[0] = boneClips;
-        clipList[1] = healthInspectorClips;
-        clipList[2] = cupboardClips;
-        clipList[3] = cuttingboardClips;
-        clipList[4] = torsoClips;
-        clipList[5] = limbClips;
-        clipList[6] = fridgeClips;
-        clipList[7] = orderClips;
-        clipList[8] = gameStateClips;
-        clipList[9] = meatClips;
-        clipList[10] = vegetableClips;
+        clipList[1] = breadClips;
+        clipList[2] = clocheClips;
+        clipList[3] = cupboardClips;
+        clipList[4] = cuttingboardClips;
+        clipList[5] = fridgeClips;
+        clipList[6] = gamestateClips;
+        clipList[7] = grillClips;
+        clipList[8] = grinderClips;
+        clipList[9] = healthInspectorClips;
+        clipList[10] = knifeClips;
+        clipList[11] = limbClips;
+        clipList[12] = meatClips;
+        clipList[13] = musicClips;
+        clipList[14] = orderClips;
+        clipList[15] = plateClips;
+        clipList[16] = handClips;
+        clipList[17] = saladClips;
+        clipList[18] = torsoClips;
+        clipList[19] = vegetableClips;
     }
 
 
